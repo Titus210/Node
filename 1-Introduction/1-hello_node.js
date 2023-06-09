c155edd47913a974777f59d1c9c96aa94d983afc
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-06-09 21:30:11 +0300

    printing first statement in node

--- 1-Introduction/1-hello_node.js
+++ 1-Introduction/1-hello_node.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+console.log("Hello World");
+
+// import required modules
+var http = require("http");
+
+// create server
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
+  })
+  .listen(8081);
+console.log("server running at http://127.0.0.1:8081/");

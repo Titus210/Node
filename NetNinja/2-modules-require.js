75da47f209d3c075563bd316a50b6a44d55f1a50
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-06-10 09:09:10 +0300

    modules and require

---
+++ NetNinja/2-modules-require.js
@@ -0,0 +1,8 @@
+// import people module
+const xyz = require("./people");
+
+console.log(xyz); // gives an empty array
+// console.log(people) // gives an error
+
+console.log(xyz.people);
+console.log(xyz.age);

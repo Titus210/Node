39fec653c0e4e30227104f1fafe076a624b62f02
devCoyo <tituskiplagat06@gmail.com>
2024-10-03 17:09:53 +0300

    inserting and exporting filees as modules using spread operators nd directly defining them in the index.js file instead of exposing them in the http folder

--- zero-to-mastery/lessons/http-module/https.js
+++ zero-to-mastery/lessons/http-module/https.js
@@ -1,11 +1,11 @@
-const internals = require('./internals')
+const {send,read} = require('./internals')
 
 // const request = require('./request');
 //const response = require('./response');
 
 function makeRequest(url, data){
-    internals.request.send(url, data);
-    return internals.response.read();
+    send(url, data);
+    return read();
 }
 
 const responseData = makeRequest('https://www.google.com');
--- zero-to-mastery/lessons/http-module/internals/index.js
+++ zero-to-mastery/lessons/http-module/internals/index.js
@@ -1,5 +1,18 @@
-// exporting internal folder as module
+/* exporting internal folder as module*/
+
+// directly import here to avoid using (response, require) in the (http.js) file
+// const request = require('./request') ;
+// const response = require('./response');
+
+/*
 module.exports = {
-    response: require('./response.js'),
-    request: require('./request.js')
+    send: request.send,
+    read: response.read,
+}
+*/
+
+// using spread operator
+module.exports ={
+    ...require("./request"),
+    ...require("./response"),
 }

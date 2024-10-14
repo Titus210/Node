24b4b643caa4d993429d28fdbf0070747238cf9c
Titus210 <tituskiplagat210@gmail.com>
2024-10-14 21:26:38 +0300

    - check if planet is habitable

--- zero-to-mastery/planet-project/index.js
+++ zero-to-mastery/planet-project/index.js
@@ -3,6 +3,14 @@ const fs = require('fs')
 
 const results = []
 
+// check if planet is habitable
+function isHabitable(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 &&
+        planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6
+}
+
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
         comment: "#",

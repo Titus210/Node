9b726b10596d6fb9c3a21e4bbd790b66f677b11a
Titus210 <tituskiplagat210@gmail.com>
2024-10-15 12:22:12 +0300

    - sending response and receiving data via urls/ endpoints

--- zero-to-mastery/lessons/http-server/index.js
+++ zero-to-mastery/lessons/http-server/index.js
@@ -3,11 +3,23 @@ const PORT = 8000;
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(JSON.stringify({
-        id: 1,
-        planet_name: 'Earth',
-    }));
+    if (req.url === '/planets') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(JSON.stringify({
+            id: 1,
+            planet_name: 'Earth',
+        }));
+    }
+    else if (req.url === "/messages") {
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>Messages</h1>')
+        res.write('<p>Message 1</p>')
+        res.write('<p>Message 2</p>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    }
 })
 
 server.listen(PORT, () => {

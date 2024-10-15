ca4704b778c565c8d421f8d9e8211507db3f4295
Titus210 <tituskiplagat210@gmail.com>
2024-10-15 10:23:05 +0300

    - printing individual planets name

--- zero-to-mastery/planet-project/index.js
+++ zero-to-mastery/planet-project/index.js
@@ -27,5 +27,8 @@ fs.createReadStream('kepler_data.csv')
     })
     .on('end', () => {
         console.log(`${habitablePlanets.length} habitable planets found!`)
+        console.log(habitablePlanets.map((planet) =>{
+            return planet['kepler_name']
+        }))
         console.log("done processing")
     });
